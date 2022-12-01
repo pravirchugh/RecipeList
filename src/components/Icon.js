b6abf683722a2267fca9d403d11d6abb08cde28d
pravirchugh <pravirchugh@yahoo.com>
2022-11-30 22:16:49 -0800

    Added stylistic changes to each recipe

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import { Grid } from "@mui/material";
+import './Icon.css';
 
 function Icon(props){
     return (
-        <>
-            <h2>{props.recipe_course}</h2>
-        </>
+        <div className='wrapperIcon'>
+            <h3><u>{props.recipe_name}</u></h3>
+            <h4>{props.recipe_course}</h4>
+        </div>
     )
    
 }
