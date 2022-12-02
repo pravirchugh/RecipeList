7699f07a50f0967de520320324e87fcfedb34a16
pravirchugh <pravirchugh@yahoo.com>
2022-12-01 22:09:01 -0800

    Added basic search bar functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import './App.css';
 import IconList from './components/IconList.js'
 import { Grid } from "@mui/material";
 
+const RecipeInfo = [
+  {name: "Chicken Tikka", course: "main"},
+  {name: "Fettuccine Alfredo", course: "main"},
+  {name: "Quesadillas", course: "main"},
+  {name: "Caesar Salad", course: "side"},
+  {name: "Pound Cake", course:"dessert"},
+  {name: "Flan", course: "dessert"},
+];
+
+function search(){
+  let text = document.getElementById("searchBar").value;
+  let found = false;
+  for(let i = 0; i < RecipeInfo.length; i++){
+    if(text == RecipeInfo[i].name){
+      alert("We have this recipe! 😊");
+      found = true;
+    }
+  }
+  if(!found){
+    alert("Sorry, we don't have that recipe at the moment!");
+  }
+ 
+}
+
 function App() {
   return (
     <>
@@ -10,7 +34,7 @@ function App() {
 
         <div style={{float: 'right', marginRight: '1.5%'}}>
           Search: <input type="text" className='sub1' id='searchBar'></input>
-          <button className="searchBtn">Enter</button>
+          <button className="searchBtn" onClick={() => search()}>Enter</button>
         </div>
 
         <br></br>
