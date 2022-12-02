dccdd1e98d0e4821193fda68e2dc42f5770e07cf
pravirchugh <pravirchugh@yahoo.com>
2022-12-01 22:03:23 -0800

    Added enter button

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ function App() {
     <>
       <div id="introduction">
         <br></br>
-        <div style={{float: 'right', marginRight: '1.5%'}}>Search: <input type="text" className='sub1' id='searchBar'></input></div>
+
+        <div style={{float: 'right', marginRight: '1.5%'}}>
+          Search: <input type="text" className='sub1' id='searchBar'></input>
+          <button className="searchBtn">Enter</button>
+        </div>
+
         <br></br>
         <h1 className='sub1' style={{textAlign: "center"}}><b><u>Recipes</u></b> 📖</h1>
         <h3 style={{marginLeft: "1.5%"}}>Welcome to my Recipe site! Here, I have organized and listed some of my favorite recipes! I hope you enjoy! 😋</h3>
